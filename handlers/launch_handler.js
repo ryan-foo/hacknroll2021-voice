204530c6c18201e539b64783e3b2d5dd73708264
zhermin <tamzhermin@gmail.com>
2021-01-08 19:21:39 +0800

    Updated

--- handlers/launch_handler.js
+++ handlers/launch_handler.js
@@ -1,28 +1,3 @@
-// hihi test
-
-const LaunchRequest_Handler =  {
-    canHandle(handlerInput) {
-        const request = handlerInput.requestEnvelope.request;
-        return request.type === 'LaunchRequest';
-    },
-    handle(handlerInput) {
-        const responseBuilder = handlerInput.responseBuilder;
-
-        let say = 'hello' + ' and welcome to ' + invocationName + ' ! Say help to hear some options.';
-
-        let skillTitle = capitalize(invocationName);
-
-
-        return responseBuilder
-            .speak(say)
-            .reprompt('try again, ' + say)
-            .withStandardCard('Welcome!',
-              'Hello!\nThis is a card for your skill, ' + skillTitle,
-               welcomeCardImg.smallImageUrl, welcomeCardImg.largeImageUrl)
-            .getResponse();
-    },
-};
-
 /** Launch Handler
  * The Launch Handler is responsible for handling the first time a user invokes the game.
  */
