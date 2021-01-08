8e7002dad9345e9cfa189acf2da8f6c636a6ec5f
xuehuitan <xuehui99@gmail.com>
2021-01-08 23:30:17 +0800

    Updated answer_handler

--- handlers/answer_handler.js
+++ handlers/answer_handler.js
@@ -15,46 +15,44 @@ module.exports = {
         const cats = ['SONG', 'MOVIE', 'PHRASES'];
         const slots = handlerInput.requestEnvelope.request.intent.slots.answer;
         const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-        //kiv round
         const round = sessionAttributes.round;
         const cat = cats[round - 1]; //1 - Song, 2 - Movie, 3 - Phrases
         const qn = utils.getId(slots).qn; //cat's qn (1, 2, 3)
         const ans = utils.getId(slots).id; //player's ans ('A')
         const player = sessionAttributes.currentPlayer - 1; //0 or 1
         const numPlayers = sessionAttributes.player; //1 or 2
-        //kiv
-        const count = sessionAttributes.currentQuestion; //increments - if currently is qn 1 then is player1, qn 2 then is player2
-        let questions = sessionAttributes.questions; //array of qn numbers to be asked
+        // const count = sessionAttributes.currentQuestion; //increments - if currently is qn 1 then is player1, qn 2 then is player2
+        let questions = sessionAttributes.questions; //randomised array of qn numbers to be asked
         let points = sessionAttributes.score;
+        let speechText = utils.checkAnswer(cat, qn, ans);
 
         // add points
-        let speechText = utils.checkAnswer(cat, qn, ans);
-        if (numPlayers === 1) { //solo
-            points += utils[cat][qn][ans]; //cat = 0, qn = '1', ans = 'A'
-        } else { //duo
-            //kiv about how to allocate alternating points
-            points[count%2] += utils[cat][qn][ans]; //1 will be player1, 0 will be player2
-            sessionAttributes.player = utils.resetPlayer(player);
-  ``    }
-        const nextQuestion = utils.getRandomQuestion(questions);
-        speechText += strings.SCORE; //-1 if the numbers are not the index
+        points[player] += utils[cat][qn][ans];
+
+        //check if end game
+        if (round === 3) {
+            speechText = utils.endGame(numPlayers, points);
+            sessionAttributes.state = 'ENDED';
+        } else {
+            if (numPlayers === 2) { //duo
+                speechText += strings['SCORE'] + points[player];
+                if (player === 1) { //player2:
+                    speechText += strings['PLAYER_1'] + strings['AGAIN'] + strings['QUESTION_' + (sessionAttributes.currentQuestion).toString()] + strings.cat[questions[count - 1]]
+                } else { //player1
+                    speechText += strings['PLAYER_2'] + strings['QUESTION_' + (sessionAttributes.currentQuestion).toString()] + strings.cat[questions[count - 1]]
+                // strings['PLAYER_2'] + strings['QUESTION_2'] // number based on cat
+                }
+                // speechText += strings['QUESTION_' + (sessionAttributes.currentQuestion).toString()] + strings.cat[questions[count - 1]]; //[nextQuestion - 1];
+                sessionAttributes.player = utils.resetPlayer(player);
+            } else { //solo
+                speechText += points[player] + strings['QUESTION_' + (sessionAttributes.currentQuestion + 2).toString()] + strings.cat[questions[count - 1]];
+            }
+        }
 
         // update sessionAttributes
         sessionAttributes.currentQuestion += 1;
-        // sessionAttributes.questions = questions;
         sessionAttributes.lastUtterance = speechText;
         handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
-
-        // reset questions array
-        questions[player].push(nextQuestion);
-
-        //end game
-        if (round === 3) {
-            speechText += strings['WIN'];
-            sessionAttributes.state = 'ENDED';
-        } else {
-            speechText += strings.cat[nextQuestion - 1];
-        }
 
         return handlerInput.responseBuilder
         .speak(speechText)
